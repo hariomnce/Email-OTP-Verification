5e18eba289ef12e17041a88f6cc8ab2ed42a9662
Hariom Sharma <hariomnce@gmail.com>
2024-02-12 10:32:31 +0530

    updated

---
+++ src/test/java/com/emailotpverification/EmailOtpVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailotpverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailOtpVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
